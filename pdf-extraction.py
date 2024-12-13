7b97ae41f80f496db6889355c63c8a1ff55ba78b
Bruno <bgurgelamaral@gmail.com>
2024-12-13 16:55:26 -0300

    some space problems have been solved

--- pdf-extraction.py
+++ pdf-extraction.py
@@ -5,6 +5,13 @@ import pdfplumber
 
 file_path = r'C:\Users\b_gur\OneDrive\Documentos\order-processing-py\pdfs\type-a\ORV Suporte Rei - Imperio dos freios 12dez.pdf'
 
+def clean_text(text):
+    # Remove espaços extras em valores monetários e normaliza os números
+    text = re.sub(r"R\$ (\d+)\s,(\d+)", r"R$ \1,\2", text)
+    text = re.sub(r"(\S+)(\d+\.\d+%)", r"\1 \2", text)
+    text = re.sub(r"(\d+)\s(\d+)", r"\1\2", text)
+    return text
+
 def extract_pdf(path):
     data = {
         "Cliente": None,
@@ -16,6 +23,8 @@ def extract_pdf(path):
         for page in pdf.pages:
             text = page.extract_text()
             text = " ".join(text.split())
+            text = clean_text(text) 
+            #print(f'texto processado: \n{text}\n {"-"*80}')
 
             if not data["Cliente"] or not data["Data"]:
                 match_cliente = re.search(r"Cliente:\s*(.+?)\s+\d{2}/\d{2}/\d{4}", text)
@@ -25,26 +34,28 @@ def extract_pdf(path):
                 if match_data:
                     data["Data"] = match_data.group(0)
 
+            #Processamento da tabela produtos
             lines = text.split("\n")
+            
             for line in lines:
-                match_product = re.match(r"(\S+)\s+\d+%?\s+\d+\s+\d+\s+R\$ ([\d,.]+)\s+R\$ ([\d,.]+)\s+R\$ ([\d,.]+)", line)
+                print(f"Processando linha: {line}")
+                # Regex ajustado para capturar código, IPI, quantidade, e valores
+                match_product = re.match(
+                    r"(\S+)\s+(\d+\.\d+%)\s+\d+\s+\d+\s+R\$ ([\d,.]+)\s+R\$ ([\d,.]+)\s+R\$ ([\d,.]+)", 
+                    line
+                )
                 if match_product:
                     produto = {
                         "Código": match_product.group(1),
-                        "Qt": match_product.group(3),
-                        "Valor s/Impostos": match_product.group(4),
-                        "IPI": match_product.group(5)
+                        "IPI": match_product.group(2),
+                        "Valor s/Impostos": f"R$ {match_product.group(3)}",
+                        "Qt": f"{match_product.group(4)}",
+                        "Total s/Impostos": f"R$ {match_product.group(5)}"
                     }
                     data["Produtos"].append(produto)
-            
-            # for page_num, page in enumerate(pdf.pages, start=1):
-            #     text = page.extract_text()
-            #     print(f"texto extraido - página {page_num}:")
-            #     print(text)
-            #     print("-"*80)
-
     return data
 
-dados = extract_pdf(file_path)
 
-print(dados)
+dados = extract_pdf(file_path)
+df = pd.DataFrame(dados)
+print(df)
